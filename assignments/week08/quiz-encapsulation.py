f04e44985ce29f337f4a7cd1101c282d1ceec532
Supawit Leelachayakul <supawit.le@ku.th>
2025-09-16 15:05:48 +0700

    week08

--- assignments/week08/quiz-encapsulation.py
+++ assignments/week08/quiz-encapsulation.py
@@ -6,4 +6,28 @@ Methods to calculate area (getArea()) and perimeter getPerimeter())
 A method to check if it's a square (isSquare())
 
 """
+class Rectangle:
+    def __init__(self,length,width):
+        self.__length = length
+        self.__width = width
+        
+    def getArea(self):
+        Area = self.__width * self.__length
+        return f"Area = {Area}"
+    
+    def getPerimeter(self):
+        Perimeter = 2 * (self.__width + self.__length)
+        return f"Perimeter = {Perimeter}"
+    
+    def  isSquare(self):
+        return self.__length == self.__width
+rect = Rectangle(5,5)
+print(rect.getArea())
+print(rect.getPerimeter())
+print(rect.isSquare())
+print("")
+rect = Rectangle(6,8)
+print(rect.getArea())
+print(rect.getPerimeter())
+print(rect.isSquare())
     
--- assignments/week08/quiz-inheritance.py
+++ assignments/week08/quiz-inheritance.py
@@ -6,3 +6,28 @@ Create a class hierarchy:
     Implement a method get_info() in both classes
 
 """
+class Vehicle:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model 
+        self.year = year
+    
+    def get_info(self):
+        return f"Brand: {self.brand}\nModel: {self.model}\nModel year: {self.year}"
+    
+class Car(Vehicle): 
+    def __init__(self, brand, model, year, number_of_doors):
+        super().__init__(brand, model, year)
+        self.doors = number_of_doors
+    def get_info(self):
+        return f"Brand: {self.brand}\nModel: {self.model}\nModel year: {self.year}\nAll door = {self.doors}"
+    
+vehicle = Vehicle("Toyota","Camry",2025)
+print(vehicle.get_info())
+
+print("")
+
+car =Car("Toyota","Crown Sport",2025,5)
+print(car.get_info())
+
+    
